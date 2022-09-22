d2ca9a89ad160d0c71ad106f7734eabf61adabcd
willcliffy <will@wolfjawstudios.com>
2022-09-21 23:16:19 -0400

    basic input. this looks fun

--- main.rs
+++ main.rs
@@ -6,6 +6,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(input_system)
         .run();
 }
 
@@ -43,4 +44,32 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn input_system(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut camera: Query<(&mut Transform, &Camera)>,
+) {
+    let (mut camera_t, _) = camera.single_mut();
+
+    let mut direction = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction -= Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        direction += Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::Up) {
+        direction += Vec3::new(0.0, 1.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::Down) {
+        direction -= Vec3::new(0.0, 1.0, 0.0);
+    }
+
+    camera_t.translation += direction * time.delta_seconds() * 10.0;
 }
